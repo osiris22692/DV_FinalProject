665b2c5c1ce59e3889ca047d038e95340fed2ad1
Addison Staples <jastaplesiv@gmail.com>
2015-12-08 10:41:53 -0600

    updated my ui.r

--- 04 Shiny/Hospital/ui.R
+++ 04 Shiny/Hospital/ui.R
@@ -7,11 +7,14 @@ require(leaflet)
 dashboardPage(
   dashboardHeader(
   ),
+  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Crosstab", tabName = "crosstab", icon = icon("dashboard")),
+      menuItem("Crosstab", tabName = "crosstab", icon = icon("dashboard"))
+      
     )
   ),
+  
   dashboardBody(
     tabItems(
       # First tab content
@@ -19,16 +22,16 @@ dashboardPage(
               actionButton(inputId = "light", label = "Light"),
               actionButton(inputId = "dark", label = "Dark"),
               sliderInput("KPI1", "KPI_Low_Max_value:", 
-                          min = 0, max = -0.1,  value = -0.1),
+                          min = -240.0, max = -0.1,  value = -0.1),
               sliderInput("KPI2", "KPI_Medium_Max_value:", 
-                          min = 0, max = 22.26,  value = 22.26),
+                          min = 0, max = 320,  value = 0),
               textInput(inputId = "title", 
                         label = "Crosstab Title",
                         value = "Operating Margin by County and Region"),
               actionButton(inputId = "clicks1",  label = "Click me"),
               plotOutput("distPlot1")
-      ),
+      )
+      
       )
     )
   )
-)
