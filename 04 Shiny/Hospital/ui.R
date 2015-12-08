10bc37ec64508c9ee2550cdce9db2e4257a0ef6f
Addison Staples <jastaplesiv@gmail.com>
2015-12-08 10:23:09 -0600

    commit my ui and server updates

--- 04 Shiny/Hospital/ui.R
+++ 04 Shiny/Hospital/ui.R
@@ -10,9 +10,6 @@ dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Crosstab", tabName = "crosstab", icon = icon("dashboard")),
-      menuItem("ScatterPlot", tabName = "scatterplot", icon = icon("th")),
-      menuItem("BarChart", tabName = "barchart", icon = icon("th")),
-      menuItem("Blending", tabName = "blending", icon = icon("th"))
     )
   ),
   dashboardBody(
@@ -22,36 +19,15 @@ dashboardPage(
               actionButton(inputId = "light", label = "Light"),
               actionButton(inputId = "dark", label = "Dark"),
               sliderInput("KPI1", "KPI_Low_Max_value:", 
-                          min = 0, max = 15.26,  value = 15.26),
+                          min = 0, max = -0.1,  value = -0.1),
               sliderInput("KPI2", "KPI_Medium_Max_value:", 
-                          min = 15.26, max = 22.26,  value = 22.26),
+                          min = 0, max = 22.26,  value = 22.26),
               textInput(inputId = "title", 
                         label = "Crosstab Title",
-                        value = "Diamonds Crosstab\nSUM_PRICE, SUM_CARAT, SUM_PRICE / SUM_CARAT"),
+                        value = "Operating Margin by County and Region"),
               actionButton(inputId = "clicks1",  label = "Click me"),
               plotOutput("distPlot1")
       ),
-      
-      # Second tab content
-      tabItem(tabName = "scatterplot",
-              actionButton(inputId = "clicks2",  label = "Click me"),
-              plotOutput("distPlot2")
-      ),
-      
-      # Third tab content
-      tabItem(tabName = "blending",
-              actionButton(inputId = "clicks3",  label = "Click me"),
-              plotOutput("distPlot3")
-      ),
-      
-      # Fourth tab content
-      tabItem(tabName = "map",
-              leafletOutput("map")
-      ),
-      
-      # Fifth tab content
-      tabItem(tabName = "table",
-              dataTableOutput("table")
       )
     )
   )
